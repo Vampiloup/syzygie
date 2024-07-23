4b5abb591641764adb258d2558cc7361cb852d20
vampiloup <github@vampiloup.com>
2024-07-23 17:02:53 +0200

    essai 3

--- Normal/Objets/CreationEtoiles.go
+++ Normal/Objets/CreationEtoiles.go
@@ -32,22 +32,3 @@ embedded_components {
     w: 1.0
   }
 }
-embedded_components {
-  id: "factory_systeme"
-  type: "factory"
-  data: "prototype: \"/Normal/Objets/Etoile.go\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
