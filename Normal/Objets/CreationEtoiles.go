9abfb4b446759825b52b607028f6ec1634de83f2
vampiloup <github@vampiloup.com>
2024-10-03 16:18:05 +0200

    name commit

--- Normal/Objets/CreationEtoiles.go
+++ Normal/Objets/CreationEtoiles.go
@@ -1,34 +1,10 @@
 components {
   id: "CreationEtoiles"
   component: "/Normal/Scripts/CreationEtoiles.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 embedded_components {
-  id: "factory"
-  type: "factory"
-  data: "prototype: \"/Normal/Objets/Etoile.go\"\n"
-  "load_dynamically: false\n"
-  "dynamic_prototype: false\n"
+  id: "factory_etoile"
+  type: "collectionfactory"
+  data: "prototype: \"/Normal/main/EtoileCarte.collection\"\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
